0c58cd70e2c93ae548ce1b4191cffc66457fd88d
isabelsousac <isabel.sousa15246@gmail.com>
2022-11-20 16:16:37 +1100

    Authorized access is now required. Application controller contains functions that handle user authenticity. Methods that were on session controller is now on user controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, :only => [:new, :create, :edit, :update]
+  resources :users, :only => [:index, :create, :update]
+  post '/login' => 'users#login'
+  get '/profile' => 'users#user_profile'
+
   resources :symptoms, :only => [:show, :update, :create, :delete]
   resources :period, :only => [:index, :show, :create, :update]
   resources :cycle 
-
-  post 'login' => 'sessions#create'
-  delete 'login' => 'sessions#destroy'
 end
